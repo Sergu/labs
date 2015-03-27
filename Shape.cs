f3b00bd9dc2e7f8df70bbfd59d819e04ffa0bc13
Sergu <sergushov.1995@mail.ru>
2015-03-27 23:21:46 +0300

    creators,shapes.Seperation functions situated in class Draw.cs

---
+++ Shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace oop1_figuer_s_hierarchy
+{
+    public abstract class Shape
+    {
+        public List<Point> points; 
+        public Pen pen;
+        public Graphics graphics;
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace oop1_figuer_s_hierarchy
+{
+    public class Square : Rectangle
+    {
+        public Square(Graphics graphics, Pen pen,List<Point> points)
+            : base(graphics,pen,points)
+        {
+        }
+    }
+}
